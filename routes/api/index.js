2252b7199a430aa0377b986da77f8d7be3089255
MBP Julius MathisonÂ <julius.mathison@outlook.com>
2022-12-20 17:02:26 -0600

    se agrega route api location

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const travellerRoutes = require('./travellerRoutes');
+const locationRoutes = require('./locationRoutes');
 
 router.use('/api/traveller', travellerRoutes);
+
+router.use('/api/location', locationRoutes);
 
 module.exports = router;
