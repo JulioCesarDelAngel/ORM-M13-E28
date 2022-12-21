27e1c67a697ffcbddeff9bf581dd1ab1cba79cb3
MBP Julius MathisonÂ <julius.mathison@outlook.com>
2022-12-21 09:42:46 -0600

    se agrega endpoint GET location

--- routes/api/locationRoutes.js
+++ routes/api/locationRoutes.js
@@ -1,18 +1,31 @@
 const router = require ('express').Router();
+const { response } = require('express');
 const { Location } = require('../../models');
 
 router.get('/', async ( request, response) => {
     try {
-        response.status(200).json({
-            "code": 200,
-            "Message": "TestOk",
-            "data": []
-        });
+            const locations = await Location.findAll();
+            response.status(200).json(locations);
     }
     catch ( error ){
         response.status(500).json( error);
     }
 
 });
+
+router.post('/', async (reuqest, response) => {
+    try {
+        response.status(200).json({
+            "code": 200,
+            "Message": "TestOk",
+            "data": []
+        });
+    
+    }
+
+    catch ( error ){
+        response.status(500).json( error );
+    }
+})
 
 module.exports = router;
